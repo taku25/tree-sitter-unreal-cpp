edea4bef5d0840c8654e5dbac15eefc82ff9c7e5
taku25 <taku36@gmail.com>
2025-09-19 15:26:08 +0900

    fix schema

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -79,7 +79,7 @@
 
 ; START UNREAL ENGINE 
 
-; UPROPERTY, UFUNCTION などのマクロ自体を紫色 (keyword) にする
+; --- マクロ自体のハイライト ---
 (uproperty_macro) @keyword
 (ufunction_macro) @keyword
 (uclass_macro) @keyword
@@ -87,24 +87,29 @@
 (uenum_macro) @keyword
 (unreal_body_macro) @keyword
 
-; UCLASS() class MyActor の MyActor の部分を型名 (type) の色にする
-(unreal_class_declaration
-  name: (type_identifier) @type.definition)
+; --- クラス名とプロパティ名のハイライト ---
+(unreal_class_declaration name: (type_identifier) @type.definition)
+(field_declaration (uproperty_macro) declarator: (field_identifier) @property)
 
-; UPROPERTY(...) int32 Health の Health の部分をプロパティの色にする
-(field_declaration
-  (uproperty_macro)
-  declarator: (field_identifier) @property)
+; --- 指定子 (Specifiers) のハイライト ---
 
+; 1. 単体の指定子 (Blueprintable, EditAnywhere など)
+(unreal_specifier (unreal_specifier_keyword) @property)
+(unreal_specifier (identifier) @property) ; タイプミスや未知の指定子用
 
+; 2. キー・バリューペアの「キー」部分 (Category, meta など)
+(unreal_specifier key: (_) @property)
 
-; 新しく定義したノードに、特別なハイライトグループを割り当てる
-(unreal_class_declaration body: (field_declaration_list (access_specifier) @constant.builtin))
-(unreal_struct_declaration body: (field_declaration_list (access_specifier) @constant.builtin))
+; 3. キー・バリューペアの「バリュー」部分 ("Input" など)
+(unreal_specifier value: (string_literal) @string)
 
-; Category="Value" のValueの部分は通常の文字列の色にする
-(unreal_specifier
-  key: (_)
-  value: (string_literal) @string)
+; --- アクセス指定子 (public:, protected:) のハイライト ---
+
+; メンバーのアクセス指定子 (public: など)
+; "field_declaration_list" の中にある "access_specifier" を探す
+(field_declaration_list (access_specifier) @keyword)
+
+; 基底クラスの継承指定子 (: public など)
+(base_class_clause (access_specifier) @keyword)
 
 ; END UNREAL ENGINE
